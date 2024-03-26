9d9722401a2e89f29be39de33402c99adb892895
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-03-26 15:28:23 +0100

    add items in homepage , and preparing the items.php page

--- index.php
+++ index.php
@@ -3,6 +3,42 @@
     session_start();
     include 'init.php';
 
+    ?>
+    <section class="cats-items my-5">
+        <div class="container">
+            <?php
+                $getCategories = query('select','Categories',['*'],NULL,NULL,'Ordering');
+                if($getCategories->rowCount() > 0){
+                    while($cat = $getCategories->fetchObject()){
+                        $getItemsToCat = query('select','Items',['*'],[$cat->CatID],['CatID'],'ItemID','DESC',4);
+                        if($getItemsToCat->rowCount() > 0){
+                            echo '<h4 class="text-center text-capitalize my-4 bg-second-color py-1 text-white rounded">'.$cat->Name.'</h4>';
+                            echo '<div class="row">';
+                                while($item = $getItemsToCat->fetchObject()){
+                                    ?>
+                                            <div class="col-md-6 col-lg-4 col-xl-3 mb-3">
+                                            <div class="card p-1" style="height:400px">
+                                                <div class="card-body">
+                                                    <div class="card-img-top text-center">
+                                                        <img src="imgs/item.jpg" alt="image of item" style="max-height:200px"/>
+                                                    </div>
+                                                    <h4 class="card-title"><?= $item->Name ?></h4>
+                                                    <p class="card-text"><?= $item->Description ?></p>
+                                                    <a href="items.php?itemid=<?= $item->ItemID ?>" class="card-link">Click Here ...</a>
+                                                    <span class="price"><?= $item->Price ?> <?= $item->Currency ?></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php
+                                }
+                            echo '</div>';
+                        }
+                    }
+                }
+            ?>
+        </div>
+    </section>
+    <?php
 
     include $template.'footer.php';
     ob_end_flush();
---
+++ items.php
@@ -0,0 +1,8 @@
+<?php
+    ob_start();
+    session_start();
+    include 'init.php';
+    
+    include $template.'footer.php';
+    ob_end_flush();
+?>
